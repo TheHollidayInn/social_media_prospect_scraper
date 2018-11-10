2669d2bc16b26d6bf271de87f4a88dea0709b5bf
Keith Holliday <keithrholliday@gmail.com>
2018-11-09 21:34:09 -0600

    Initial wire up of scrapper to queue

--- website-contact-scraper/producer.js
+++ website-contact-scraper/producer.js
@@ -3,7 +3,7 @@ const Constants = require('./constants');
 
 const queue = new Queue(Constants.WEBSITE_CONTACT_SCRAPER_QUEUE);
 
-const job = queue.createJob({x: 2, y: 3})
+const job = queue.createJob({ x: 2, y: 3 })
 job.save();
 job.on('succeeded', (result) => {
   console.log(`Received result for job ${job.id}: ${result}`);
--- website-contact-scraper/worker.js
+++ website-contact-scraper/worker.js
@@ -1,5 +1,7 @@
 const Queue = require('bee-queue');
+
 const Constants = require('./constants');
+const { startScrapingFromURL } = require('../website-contact-scraper');
 
 const queue = new Queue(
   Constants.WEBSITE_CONTACT_SCRAPER_QUEUE,
@@ -8,11 +10,12 @@ const queue = new Queue(
     //   host: 'somewhereElse'
     // },
     isWorker: true,
-  }
+  },
 );
 
 // Process jobs from as many servers or processes as you like
-queue.process(function (job, done) {
+queue.process((job, done) => {
+  startScrapingFromURL('http://test.com');
   console.log(`Processing job ${job.id}`);
   return done(null, job.data.x + job.data.y);
 });
